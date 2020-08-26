83cda0ca59db2f66fd80a881bc658c5baa19ed95
Suraj Wate <suraj.wate@gmail.com>
2020-08-26 13:39:29 +0530

    Stage 3: Make your choice

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,7 +1,12 @@
+import random
+
 print("H A N G M A N")
 word = input("Guess the word:")
+word_list = ['python', 'java', 'kotlin', 'javascript']
 
-if word == 'python':
+random_word = random.choice(word_list)
+
+if word == random_word:
     print("You survived!")
 else:
     print("You are hanged!")
