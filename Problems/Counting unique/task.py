fb12ba68f841e6352dd449661cdbfd2e9ced488b
Suraj Wate <suraj.wate@gmail.com>
2020-08-26 19:52:32 +0530

    Counting unique

---
+++ Problems/Counting unique/task.py
@@ -0,0 +1,12 @@
+# The following code is needed for us to check your answer, do not modify it, please.
+students = json.loads(input())
+Belov = students['Belov']
+Smith = students['Smith']
+Sarada = students['Sarada']
+
+# Your code here. Work with the variables 'Belov', 'Smith', and 'Sarada'
+
+sub = set(Belov)
+sub.update(Smith)
+sub.update(Sarada)
+print(len(sub))
